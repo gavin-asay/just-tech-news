16b06f2e848d1c9d68fc64e2ff7ebc425af539e5
Gavin Asay <gavin.asay@gmail.com>
2021-02-11 03:45:06 -0700

    vote relations

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 router.get('/', (req, res) => {
 	// Access our User model and run .findAll() method
@@ -15,7 +15,21 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
 	User.findOne({
-		attributes: { exclude: ['password'] },
+		attributes: {
+			exclude: ['password'],
+			include: [
+				{
+					model: Post,
+					attributes: ['id', 'title', 'post_url', 'created_at'],
+				},
+				{
+					model: Post,
+					attributes: ['title'],
+					through: Vote,
+					as: 'voted_posts',
+				},
+			],
+		},
 		where: {
 			id: req.params.id,
 		},
